2d441b73cd84d04ac596fd737baee86492c29596
Dannel Albert <cuebix@gmail.com>
2023-11-03 17:06:43 -0400

    feat: add first astro island

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,25 @@
+import { useState } from "preact/hooks";
+
+export type GreetingProps = {
+  messages: string[];
+};
+
+export const Greeting = (props: GreetingProps) => {
+  const { messages } = props;
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <>
+      <div>
+        <h3>{greeting}! Thank you for visiting!</h3>
+        <button onClick={() => setGreeting(randomMessage())}>
+          New Greeting
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Greeting;
